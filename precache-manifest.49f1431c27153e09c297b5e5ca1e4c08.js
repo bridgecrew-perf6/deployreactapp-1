6e4d89851909306f25e3aac162e748289cb72797
harshtictac <97781275+harshtictac@users.noreply.github.com>
2022-01-22 20:22:40 +0530

    Add files via upload

---
+++ precache-manifest.49f1431c27153e09c297b5e5ca1e4c08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc03d98f2df0ff0f451d00d14f4f1eb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01ff388e4cc71df7bff6",
+    "url": "/static/css/main.6c6bcf4a.chunk.css"
+  },
+  {
+    "revision": "7d16fb448d2de5864145",
+    "url": "/static/js/2.2c97ca31.chunk.js"
+  },
+  {
+    "revision": "1611c050afd48d2ff311",
+    "url": "/static/js/3.26aa4bf2.chunk.js"
+  },
+  {
+    "revision": "01ff388e4cc71df7bff6",
+    "url": "/static/js/main.f4c3ee03.chunk.js"
+  },
+  {
+    "revision": "193e70c58e5af559b55c",
+    "url": "/static/js/runtime~main.88519e64.js"
+  }
+]);
